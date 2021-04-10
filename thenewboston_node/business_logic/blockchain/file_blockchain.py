15d334005837a5112b6ee2e1dd7326cf43d95aba
Dmitry Mugtasimov <dmugtasimov@gmail.com>
2021-04-10 21:52:25 +0300

    Persist block implementation for file blockchain

--- thenewboston_node/business_logic/blockchain/file_blockchain.py
+++ thenewboston_node/business_logic/blockchain/file_blockchain.py
@@ -2,8 +2,6 @@ import logging
 import os.path
 from typing import Generator
 
-import msgpack
-
 from thenewboston_node.business_logic.models.account_root_file import AccountRootFile
 from thenewboston_node.business_logic.models.block import Block
 from thenewboston_node.business_logic.storages.file_system import FileSystemStorage
@@ -12,7 +10,10 @@ from .base import BlockchainBase
 
 logger = logging.getLogger(__name__)
 
+# TODO(dmu) LOW: Move these constants to configuration files
 ORDER_OF_ACCOUNT_ROOT_FILE = 10
+ORDER_OF_BLOCK = 20
+BLOCK_CHUNK_SIZE = 1000
 
 
 class FileBlockchain(BlockchainBase):
@@ -30,20 +31,37 @@ class FileBlockchain(BlockchainBase):
         if validate:
             self.validate()
 
+    # Account root files methods
+    def persist_account_root_file(self, account_root_file: AccountRootFile):
+        last_block_number = account_root_file.last_block_number
+
+        prefix = ('.' if last_block_number is None else str(last_block_number)).zfill(ORDER_OF_ACCOUNT_ROOT_FILE)
+        file_path = os.path.join(self.account_root_files_directory, f'{prefix}-arf.msgpack')
+        self.storage.save(file_path, account_root_file.to_messagepack(), is_final=True)
+
+    def iter_account_root_files(self) -> Generator[AccountRootFile, None, None]:
+        storage = self.storage
+        for file_path in storage.list_directory(self.account_root_files_directory):
+            yield AccountRootFile.from_messagepack(storage.load(file_path))
+
+    # Blocks methods
+    def persist_block(self, block: Block):
+        block_number = block.message.block_number
+        chunk_number, offset = divmod(block_number, BLOCK_CHUNK_SIZE)
+
+        chunk_block_number_start = chunk_number * BLOCK_CHUNK_SIZE
+        start_str = str(chunk_block_number_start).zfill(ORDER_OF_BLOCK)
+
+        chunk_block_number_end = chunk_block_number_start + BLOCK_CHUNK_SIZE - 1
+        end_str = str(chunk_block_number_end).zfill(ORDER_OF_BLOCK)
+
+        is_final = offset == BLOCK_CHUNK_SIZE - 1
+
+        file_path = os.path.join(self.blocks_directory, f'{start_str}-{end_str}-block-chunk.msgpack')
+        self.storage.append(file_path, block.to_messagepack(), is_final=is_final)
+
     def iter_blocks(self) -> Generator[Block, None, None]:
         storage = self.storage
         for file_path in storage.list_directory(self.blocks_directory):
             assert file_path
             yield NotImplemented
-
-    def iter_account_root_files(self) -> Generator[AccountRootFile, None, None]:
-        storage = self.storage
-        for file_path in storage.list_directory(self.account_root_files_directory):
-            yield AccountRootFile.from_compact_dict(msgpack.unpackb(storage.load(file_path)))
-
-    def add_account_root_file(self, account_root_file: AccountRootFile):
-        last_block_number = account_root_file.last_block_number
-
-        prefix = ('.' if last_block_number is None else str(last_block_number)).zfill(ORDER_OF_ACCOUNT_ROOT_FILE)
-        file_path = os.path.join(self.account_root_files_directory, f'{prefix}-arf.msgpack')
-        self.storage.save(file_path, msgpack.packb(account_root_file.to_compact_dict()), is_final=True)

301a9a1073f033e86ea2d2ba85819ed1475092de
Dmitry Mugtasimov <dmugtasimov@gmail.com>
2021-06-20 02:10:57 +0300

    Unittest to test docs generation

--- thenewboston_node/business_logic/tests/test_docs.py
+++ thenewboston_node/business_logic/tests/test_docs.py
@@ -1,3 +1,5 @@
+import subprocess
+
 from thenewboston_node.business_logic.docs.impl import get_signed_change_request_message_child_models
 from thenewboston_node.business_logic.docs.samples import SamplesFactory
 from thenewboston_node.business_logic.models.signed_change_request_message import (
@@ -17,3 +19,10 @@ def test_get_sample_block_map():
     block_map = SamplesFactory().get_sample_block_map()
     assert block_map.keys() == known_signed_change_request_message_classes
     assert len(set(id(item) for item in block_map.values())) == len(block_map.values())
+
+
+def test_can_generate_docs():
+    result = subprocess.run(['make', 'docs-html-test'], capture_output=True, text=True)
+    print(result.stdout)
+    print(result.stderr)
+    assert result.returncode == 0

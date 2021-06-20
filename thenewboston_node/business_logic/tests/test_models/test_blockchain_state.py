4ab46d4f59a97530d92283238978cfeea8993f99
Dmitry Mugtasimov <dmugtasimov@gmail.com>
2021-06-20 20:38:53 +0300

    Covert alpha account root file to blockchain state CLI tool

--- thenewboston_node/business_logic/tests/test_models/test_blockchain_state.py
+++ thenewboston_node/business_logic/tests/test_models/test_blockchain_state.py
@@ -4,8 +4,7 @@ from hashlib import sha3_256
 def test_normalized_blockchain_state(blockchain_genesis_state):
     assert blockchain_genesis_state.get_normalized() == (
         b'{"account_states":{"4d3cf1d9e4547d324de2084b568f807ef12045075a7a01b8bec1e7f013fc3732":{'
-        b'"balance":281474976710656,'
-        b'"balance_lock":"4d3cf1d9e4547d324de2084b568f807ef12045075a7a01b8bec1e7f013fc3732"'
+        b'"balance":281474976710656'
         b'}}}'
     )
 
@@ -13,8 +12,7 @@ def test_normalized_blockchain_state(blockchain_genesis_state):
 def test_get_next_block_identifier(blockchain_genesis_state):
     assert blockchain_genesis_state.get_next_block_identifier() == sha3_256(
         b'{"account_states":{"4d3cf1d9e4547d324de2084b568f807ef12045075a7a01b8bec1e7f013fc3732":{'
-        b'"balance":281474976710656,'
-        b'"balance_lock":"4d3cf1d9e4547d324de2084b568f807ef12045075a7a01b8bec1e7f013fc3732"'
+        b'"balance":281474976710656'
         b'}}}'
     ).digest().hex()
 

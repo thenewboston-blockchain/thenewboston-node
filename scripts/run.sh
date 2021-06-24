ab00600c0c73a0ffb39381e8f877a7d8541659f9
Dmitry Mugtasimov <dmugtasimov@gmail.com>
2021-06-24 20:45:23 +0300

    Dockerization fixup for failover account root file

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,9 +9,13 @@ poetry run python -m thenewboston_node.manage collectstatic
 # TODO(dmu) MEDIUM: Unhardcode /var/lib/blockchain
 export BLOCKCHAIN_PATH=/var/lib/blockchain
 if [ ! "$(ls -A $BLOCKCHAIN_PATH)" ]; then
-  poetry run python -m thenewboston_node.manage convert_account_root_file $ARF_URL
-  if [ $? -ne 0 ]; then
+  echo 'Downloading the latest alpha account root file...'
+  poetry run python -m thenewboston_node.manage convert_account_root_file ${ARF_URL} || true
+  if [ ! "$(ls -A $BLOCKCHAIN_PATH)" ]; then
+    echo 'Could not download the latest alpha account root file, using build time copy as failover'
     cp -R /opt/project/blockchain/* $BLOCKCHAIN_PATH
+  else
+    echo 'Downloaded the latest alpha account root file'
   fi
 fi
 

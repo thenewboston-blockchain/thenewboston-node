0e09dc62b350628362f5c8384a9af9c36268c502
Dmitry Mugtasimov <dmugtasimov@gmail.com>
2021-07-09 17:44:43 +0300

    Docker login

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,18 @@ set -e
 wget https://raw.githubusercontent.com/thenewboston-developers/thenewboston-node/master/docker-compose.yml -O docker-compose.yml
 
 if [ ! -f .env ]; then
-cat <<EOF > .env
+  cat <<EOF > .env
 THENEWBOSTON_NODE_SECRET_KEY=$(dd bs=48 count=1 if=/dev/urandom | base64)
 EOF
 fi
+
+# Support github actions deploy as well as manual deploy
+if [[ -z $GITHUB_USERNAME || -z $GITHUB_PASSWORD ]] then
+  docker login docker.pkg.github.com
+else
+  # Avoid printing the password
+  { docker login -u $GITHUB_USERNAME -p $GITHUB_PASSWORD docker.pkg.github.com; } > /dev/null 2> /dev/null
+fi
+
+docker-compose up -d
+docker logout

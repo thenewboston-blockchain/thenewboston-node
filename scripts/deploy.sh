33089fb10d03154fd3ead6f0905d4dee79052661
Dmitry Mugtasimov <dmugtasimov@gmail.com>
2021-07-10 15:43:05 +0300

    fixup

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,10 +19,13 @@ fi
 
 wget https://raw.githubusercontent.com/thenewboston-developers/thenewboston-node/master/docker-compose.yml -O docker-compose.yml
 
+test -f .env || touch .env
 grep -o THENEWBOSTON_NODE_SECRET_KEY .env || echo "THENEWBOSTON_NODE_SECRET_KEY=$(dd bs=48 count=1 if=/dev/urandom | base64)" >> .env
-grep -o THENEWBOSTON_NODE_NODE_SIGNING_KEY .env || echo "THENEWBOSTON_NODE_NODE_SIGNING_KEY=$(docker-compose run --rm node poetry run python -m thenewboston_node.manage generate_signing_key)" >> .env
-grep -o START_NODE_ARGS .env && sed -i "s/START_NODE_ARGS=.*/START_NODE_ARGS=${START_NODE_ARGS}/" .env || echo "START_NODE_ARGS=${START_NODE_ARGS}" >> .env
 
 docker-compose pull
+
+grep -o START_NODE_ARGS .env && sed -i "s/START_NODE_ARGS=.*/START_NODE_ARGS=${START_NODE_ARGS}/" .env || echo "START_NODE_ARGS=${START_NODE_ARGS}" >> .env
+grep -o THENEWBOSTON_NODE_NODE_SIGNING_KEY .env || echo "THENEWBOSTON_NODE_NODE_SIGNING_KEY=$(docker-compose --log-level CRITICAL run --rm node poetry run python -m thenewboston_node.manage generate_signing_key)" >> .env
+
 docker-compose up -d --force-recreate
 docker logout $DOCKER_REGISTRY_HOST

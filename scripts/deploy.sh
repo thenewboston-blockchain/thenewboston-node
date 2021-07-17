f741aab47b9129329b1efe6489c25f4d1cd142a9
Dmitry Mugtasimov <dmugtasimov@gmail.com>
2021-07-17 17:31:39 +0300

    Clear blockchain only on redeploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,9 +3,9 @@
 set -e
 
 DOCKER_REGISTRY_HOST=docker.pkg.github.com
-INITIALIZE_BLOCKCHAIN_ARGS="${INITIALIZE_BLOCKCHAIN_ARGS:-$1}"
-GITHUB_USERNAME="${GITHUB_USERNAME:-$2}"
-GITHUB_PASSWORD="${GITHUB_PASSWORD:-$3}"
+GITHUB_USERNAME="${GITHUB_USERNAME:-$1}"
+GITHUB_PASSWORD="${GITHUB_PASSWORD:-$2}"
+CLEAR_BLOCKCHAIN="${CLEAR_BLOCKCHAIN:-True}"
 
 docker logout docker.pkg.github.com
 
@@ -22,13 +22,15 @@ fi
 wget https://raw.githubusercontent.com/thenewboston-developers/thenewboston-node/master/docker-compose.yml -O docker-compose.yml
 
 test -f .env || touch .env
-grep -o THENEWBOSTON_NODE_SECRET_KEY .env || echo "THENEWBOSTON_NODE_SECRET_KEY=$(dd bs=48 count=1 if=/dev/urandom | base64)" >> .env
+grep -q -o THENEWBOSTON_NODE_SECRET_KEY .env || echo "THENEWBOSTON_NODE_SECRET_KEY=$(dd bs=48 count=1 if=/dev/urandom | base64)" >> .env
 
 docker-compose pull
 
-# This way we pass INITIALIZE_BLOCKCHAIN_ARGS to `run.sh` when started with docker-compose
-grep -o INITIALIZE_BLOCKCHAIN_ARGS .env && sed -i "s/INITIALIZE_BLOCKCHAIN_ARGS=.*/INITIALIZE_BLOCKCHAIN_ARGS=${INITIALIZE_BLOCKCHAIN_ARGS}/" .env || echo "INITIALIZE_BLOCKCHAIN_ARGS=${INITIALIZE_BLOCKCHAIN_ARGS}" >> .env
-grep -o THENEWBOSTON_NODE_NODE_SIGNING_KEY .env || echo "THENEWBOSTON_NODE_NODE_SIGNING_KEY=$(docker-compose --log-level CRITICAL run --rm node poetry run python -m thenewboston_node.manage generate_signing_key)" >> .env
+grep -q -o THENEWBOSTON_NODE_NODE_SIGNING_KEY .env || echo "THENEWBOSTON_NODE_NODE_SIGNING_KEY=$(docker-compose --log-level CRITICAL run --rm node poetry run python -m thenewboston_node.manage generate_signing_key)" >> .env
+
+if [ "$CLEAR_BLOCKCHAIN" == "True" ]; then
+  docker-compose run -rm node poetry run python -m thenewboston_node.manage clear_blockchain
+fi
 
 docker-compose up -d --force-recreate
 docker logout $DOCKER_REGISTRY_HOST
@@ -36,13 +38,13 @@ docker logout $DOCKER_REGISTRY_HOST
 counter=0
 until $(curl --output /dev/null --silent --head --fail http://127.0.0.1:8555/api/v1/nodes/self/); do
     counter=$(($counter + 1))
-    if [ ${counter} -ge 6 ];then
+    if [ ${counter} -ge 12 ];then
       echo 'Unable to start node'
       exit 1
     fi
 
-    echo 'Node has not started yet, waiting 5 seconds for retry'
+    echo "Node has not started yet, waiting 5 seconds for retry (${counter})"
     sleep 5
 done
 
-echo 'Node is up and running'
+echo "Node is up and running'

280d1fe164ea73245a5787528abff58134735803
Dmitry Mugtasimov <dmugtasimov@gmail.com>
2021-07-24 20:40:52 +0300

    Port fixup

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,4 +5,4 @@ set -e
 # We need to collect static files to make WhiteNoise work (and we need to do it here)
 # TODO(dmu) LOW: Collect static once, not on every run (we need to have named volume for it)
 poetry run python -m thenewboston_node.manage collectstatic
-poetry run daphne -b 0.0.0.0:8555 thenewboston_node.project.asgi:application
+poetry run daphne -b 0.0.0.0 -p 8555 thenewboston_node.project.asgi:application

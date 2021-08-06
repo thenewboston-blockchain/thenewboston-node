8e95a48a0cbae1b56ebf19253b86ebc72a38acc3
Dmitry Mugtasimov <dmugtasimov@gmail.com>
2021-08-06 23:23:16 +0300

    Fix test_can_add_block

--- thenewboston_node/business_logic/tests/fixtures/network.py
+++ thenewboston_node/business_logic/tests/fixtures/network.py
@@ -5,8 +5,17 @@ from thenewboston_node.business_logic.utils.network import make_own_node
 
 
 @pytest.fixture
-def primary_validator(primary_validator_key_pair):
-    return make_own_node(identifier=primary_validator_key_pair.public, network_addresses=['http://localhost'])
+def node_fee_amount():
+    return 3
+
+
+@pytest.fixture
+def primary_validator(primary_validator_key_pair, node_fee_amount):
+    return make_own_node(
+        identifier=primary_validator_key_pair.public,
+        network_addresses=['http://localhost'],
+        fee_amount=node_fee_amount
+    )
 
 
 @pytest.fixture
--- thenewboston_node/business_logic/tests/test_file_blockchain/test_add_block.py
+++ thenewboston_node/business_logic/tests/test_file_blockchain/test_add_block.py
@@ -2,11 +2,10 @@ from unittest.mock import patch
 
 import pytest
 
-from thenewboston_node.business_logic.blockchain.file_blockchain import FileBlockchain
 from thenewboston_node.business_logic.exceptions import ValidationError
 from thenewboston_node.business_logic.models.block import Block
 from thenewboston_node.business_logic.node import get_node_signing_key
-from thenewboston_node.core.utils.cryptography import KeyPair
+from thenewboston_node.core.utils.cryptography import KeyPair, derive_public_key
 
 
 def test_block_chunk_is_rotated(
@@ -117,76 +116,77 @@ def test_cannot_add_block_twice(
         blockchain.add_block(block)
 
 
-@pytest.mark.skip('fails')
 def test_can_add_block(
-    blockchain_directory,
-    blockchain_genesis_state,
+    file_blockchain,
     treasury_account_key_pair: KeyPair,
     user_account_key_pair: KeyPair,
-    node_key_pair: KeyPair,
     preferred_node,
 ):
-    blockchain = FileBlockchain(base_directory=blockchain_directory)
-    blockchain.add_blockchain_state(blockchain_genesis_state)
-    blockchain.validate()
+    blockchain = file_blockchain
 
-    treasury_account = treasury_account_key_pair.public
-    treasury_initial_balance = blockchain.get_account_current_balance(treasury_account)
+    treasury_account_number = treasury_account_key_pair.public
+    treasury_initial_balance = blockchain.get_account_current_balance(treasury_account_number)
     assert treasury_initial_balance is not None
 
-    user_account = user_account_key_pair.public
+    user_account_number = user_account_key_pair.public
     primary_validator = blockchain.get_primary_validator()
     assert primary_validator
-    pv_account = primary_validator.identifier
-    assert pv_account
-    node_account = node_key_pair.public
+    pv_account_number = primary_validator.identifier
+    assert pv_account_number
+    preferred_node_account_number = preferred_node.identifier
 
-    total_fees = 1 + 4
+    assert primary_validator.fee_amount > 0
+    assert preferred_node.fee_amount > 0
+    assert primary_validator.fee_amount != preferred_node.fee_amount
+    total_fees = primary_validator.fee_amount + preferred_node.fee_amount
 
-    node_signing_key = get_node_signing_key()
+    pv_signing_key = get_node_signing_key()
+    assert derive_public_key(pv_signing_key) == pv_account_number
     block0 = Block.create_from_main_transaction(
         blockchain=blockchain,
-        recipient=user_account,
+        recipient=user_account_number,
         amount=30,
         request_signing_key=treasury_account_key_pair.private,
-        pv_signing_key=node_signing_key,
+        pv_signing_key=pv_signing_key,
         preferred_node=preferred_node,
     )
     blockchain.add_block(block0)
-    assert blockchain.get_account_current_balance(user_account) == 30
-    assert blockchain.get_account_current_balance(treasury_account) == treasury_initial_balance - 30 - total_fees
-    assert blockchain.get_account_current_balance(node_account) == 1
-    assert blockchain.get_account_current_balance(pv_account) == 4
+    assert blockchain.get_account_current_balance(user_account_number) == 30
+    assert blockchain.get_account_current_balance(
+        treasury_account_number
+    ) == treasury_initial_balance - 30 - total_fees
+    assert blockchain.get_account_current_balance(preferred_node_account_number) == preferred_node.fee_amount
+    assert blockchain.get_account_current_balance(pv_account_number) == primary_validator.fee_amount
 
     with pytest.raises(ValidationError, match='Block number must be equal to next block number.*'):
         blockchain.add_block(block0)
 
     block1 = Block.create_from_main_transaction(
         blockchain=blockchain,
-        recipient=user_account,
+        recipient=user_account_number,
         amount=10,
         request_signing_key=treasury_account_key_pair.private,
-        pv_signing_key=node_signing_key,
+        pv_signing_key=pv_signing_key,
         preferred_node=preferred_node,
     )
     blockchain.add_block(block1)
-    assert blockchain.get_account_current_balance(user_account) == 40
-    assert blockchain.get_account_current_balance(treasury_account
+    assert blockchain.get_account_current_balance(user_account_number) == 40
+    assert blockchain.get_account_current_balance(treasury_account_number
                                                   ) == (treasury_initial_balance - 30 - 10 - 2 * total_fees)
-    assert blockchain.get_account_current_balance(node_account) == 1 * 2
-    assert blockchain.get_account_current_balance(pv_account) == 4 * 2
+    assert blockchain.get_account_current_balance(preferred_node_account_number) == preferred_node.fee_amount * 2
+    assert blockchain.get_account_current_balance(pv_account_number) == primary_validator.fee_amount * 2
 
     block2 = Block.create_from_main_transaction(
         blockchain=blockchain,
-        recipient=treasury_account,
+        recipient=treasury_account_number,
         amount=5,
         request_signing_key=user_account_key_pair.private,
-        pv_signing_key=node_signing_key,
+        pv_signing_key=pv_signing_key,
         preferred_node=preferred_node,
     )
     blockchain.add_block(block2)
-    assert blockchain.get_account_current_balance(user_account) == 40 - 5 - total_fees
-    assert blockchain.get_account_current_balance(treasury_account
+    assert blockchain.get_account_current_balance(user_account_number) == 40 - 5 - total_fees
+    assert blockchain.get_account_current_balance(treasury_account_number
                                                   ) == (treasury_initial_balance - 30 - 10 + 5 - 2 * total_fees)
-    assert blockchain.get_account_current_balance(node_account) == 1 * 3
-    assert blockchain.get_account_current_balance(pv_account) == 4 * 3
+    assert blockchain.get_account_current_balance(preferred_node_account_number) == preferred_node.fee_amount * 3
+    assert blockchain.get_account_current_balance(pv_account_number) == primary_validator.fee_amount * 3

063da0b4b1d6df6fa88d530c82e0607b1710318f
Dmitry Mugtasimov <dmugtasimov@gmail.com>
2021-08-14 16:51:07 +0300

    test_block_chunk_is_rotated_real_file_blockchain

--- thenewboston_node/business_logic/tests/test_file_blockchain/test_add_block.py
+++ thenewboston_node/business_logic/tests/test_file_blockchain/test_add_block.py
@@ -1,3 +1,4 @@
+import os.path
 from unittest.mock import patch
 
 import pytest
@@ -59,6 +60,120 @@ def test_block_chunk_is_rotated(
 
     assert blockchain.block_storage.files.keys() == {file1, file2}
     assert blockchain.block_storage.finalized == {file1, file2}
+
+
+def test_block_chunk_is_rotated_real_file_blockchain(
+    file_blockchain, user_account, treasury_account_signing_key, preferred_node
+):
+    blockchain = file_blockchain
+
+    signing_key = treasury_account_signing_key
+    node_signing_key = get_node_signing_key()
+
+    with patch.object(blockchain, 'block_chunk_size', 2), patch.object(blockchain.block_storage, 'compressors', ()):
+        block0 = Block.create_from_main_transaction(
+            blockchain=blockchain,
+            recipient=user_account,
+            amount=10,
+            request_signing_key=signing_key,
+            pv_signing_key=node_signing_key,
+            preferred_node=preferred_node,
+        )
+        blockchain.add_block(block0)
+
+        assert os.path.isfile(
+            os.path.join(
+                blockchain.base_directory,
+                'blocks/0/0/0/0/0/0/0/0/00000000000000000000-00000000000000000000-block-chunk.msgpack'
+            )
+        )
+
+        block1 = Block.create_from_main_transaction(
+            blockchain=blockchain,
+            recipient=user_account,
+            amount=10,
+            request_signing_key=signing_key,
+            pv_signing_key=node_signing_key,
+            preferred_node=preferred_node,
+        )
+        blockchain.add_block(block1)
+
+        assert not os.path.isfile(
+            os.path.join(
+                blockchain.base_directory,
+                'blocks/0/0/0/0/0/0/0/0/00000000000000000000-00000000000000000000-block-chunk.msgpack'
+            )
+        )
+        assert os.path.isfile(
+            os.path.join(
+                blockchain.base_directory,
+                'blocks/0/0/0/0/0/0/0/0/00000000000000000000-00000000000000000001-block-chunk.msgpack'
+            )
+        )
+
+        block2 = Block.create_from_main_transaction(
+            blockchain=blockchain,
+            recipient=user_account,
+            amount=30,
+            request_signing_key=signing_key,
+            pv_signing_key=node_signing_key,
+            preferred_node=preferred_node,
+        )
+        blockchain.add_block(block2)
+
+        assert not os.path.isfile(
+            os.path.join(
+                blockchain.base_directory,
+                'blocks/0/0/0/0/0/0/0/0/00000000000000000000-00000000000000000000-block-chunk.msgpack'
+            )
+        )
+        assert os.path.isfile(
+            os.path.join(
+                blockchain.base_directory,
+                'blocks/0/0/0/0/0/0/0/0/00000000000000000000-00000000000000000001-block-chunk.msgpack'
+            )
+        )
+        assert os.path.isfile(
+            os.path.join(
+                blockchain.base_directory,
+                'blocks/0/0/0/0/0/0/0/0/00000000000000000002-00000000000000000002-block-chunk.msgpack'
+            )
+        )
+
+        block3 = Block.create_from_main_transaction(
+            blockchain=blockchain,
+            recipient=user_account,
+            amount=50,
+            request_signing_key=signing_key,
+            pv_signing_key=node_signing_key,
+            preferred_node=preferred_node,
+        )
+        blockchain.add_block(block3)
+
+        assert not os.path.isfile(
+            os.path.join(
+                blockchain.base_directory,
+                'blocks/0/0/0/0/0/0/0/0/00000000000000000000-00000000000000000000-block-chunk.msgpack'
+            )
+        )
+        assert os.path.isfile(
+            os.path.join(
+                blockchain.base_directory,
+                'blocks/0/0/0/0/0/0/0/0/00000000000000000000-00000000000000000001-block-chunk.msgpack'
+            )
+        )
+        assert not os.path.isfile(
+            os.path.join(
+                blockchain.base_directory,
+                'blocks/0/0/0/0/0/0/0/0/00000000000000000002-00000000000000000002-block-chunk.msgpack'
+            )
+        )
+        assert os.path.isfile(
+            os.path.join(
+                blockchain.base_directory,
+                'blocks/0/0/0/0/0/0/0/0/00000000000000000002-00000000000000000003-block-chunk.msgpack'
+            )
+        )
 
 
 def test_block_is_appended(

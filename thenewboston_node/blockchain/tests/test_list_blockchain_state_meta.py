1e9aca34262072cf91cf83dbd4434003c88f8e84
Andrey Bortnikov <and.bortnik@gmail.com>
2021-08-24 14:20:23 +0300

    Blockchain state meta API: support memory blockchain

--- thenewboston_node/blockchain/tests/test_list_blockchain_state_meta.py
+++ thenewboston_node/blockchain/tests/test_list_blockchain_state_meta.py
@@ -1,14 +1,15 @@
+from rest_framework import status
+
 from thenewboston_node.business_logic.tests.base import force_blockchain
 
 API_V1_LIST_BLOCKCHAIN_STATE_URL = '/api/v1/blockchain-states-meta/'
 
-# @pytest.mark.usefixtures('memory_blockchain')
-# def test_only_file_blockchain_supported(api_client):
-#     response = api_client.get(API_V1_LIST_BLOCKCHAIN_STATE_URL)
-#
-#     # TODO(dmu) CRITICAL: Refactor test
-#     assert response.status_code == 501
-#     assert response.json() == {'detail': 'Not implemented.'}
+
+def test_memory_blockchain_supported(api_client, memory_blockchain):
+    with force_blockchain(memory_blockchain):
+        response = api_client.get(API_V1_LIST_BLOCKCHAIN_STATE_URL)
+
+    assert response.status_code == status.HTTP_200_OK
 
 
 def test_can_list_blockchain_state_meta(api_client, file_blockchain_with_two_blockchain_states):

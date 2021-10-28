1e6d20267a0bae5f4f8e4e16db472caf4477072a
bucky <buckyroberts@gmail.com>
2021-10-28 15:45:31 -0400

    Linting

--- thenewboston_node/project/urls.py
+++ thenewboston_node/project/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 import thenewboston_node.accounts.urls
